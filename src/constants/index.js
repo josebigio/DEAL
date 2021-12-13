3015af31f426c1658a6015ec10c60401f572333b
Jose Bigio <jbbigio@amazon.com>
2021-12-13 10:00:25 -0500

    added prod build

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://deal-backend-dev.us-east-1.elasticbeanstalk.com"
+export const BASE_API_URL = "https://api.deal-application.com/api"
 export const API_UPLOAD_PATH = `${BASE_API_URL}/uploadReel`
 export const API_GET_ALL_REELS_PATH = `${BASE_API_URL}/getAllReels`
 export const ALL_MEETUPS = `${BASE_API_URL}/allMeetups`
